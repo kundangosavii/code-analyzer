bd9a1e8f5dc026063850cc8b802126d7ec87707c
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-17 19:43:10 +0530

    Implemented controller for the graph visualization

--- src/api/controller.js
+++ src/api/controller.js
@@ -128,9 +128,32 @@ const getGraphController = (req, res, repoId) => {
     }
 }
 
+const getGraphWithNodeAndEdgeController = (req, res, repoId) => {
+    try {
+        const repoId = req.query.repoId;
+
+        const repoName = repoId.split("_").slice(0, -1).join("_");
+
+        fs.readFile(`C:/code-analyser/repos/${repoName}/graphNodesEdges.json`, 'utf-8', (err, data) => {
+            if (err) {
+                console.error('Error reading Graph file:', err);
+                return res.status(500).json({ message: 'An error occurred while fetching graph data.' });
+            }
+            
+            const graphNodeEdges = JSON.parse(data);
+            res.status(200).json(graphNodeEdges);
+        });
+    }
+    catch (error) {
+        console.error('Error fetching graph data:', error);
+        res.status(500).json({ message: 'An error occurred while fetching graph data.' });
+    }
+}
+
 export { 
     analyzeController, 
     getInsightsController,
     getReadableInsightsController,
-    getGraphController
+    getGraphController,
+    getGraphWithNodeAndEdgeController
 }
