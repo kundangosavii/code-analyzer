1b126824fc5e00a034859e6989819e8fea957e42
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-22 12:25:40 +0530

    Implemented function to save circular dependency information

--- src/core/saveInsights.js
+++ src/core/saveInsights.js
@@ -135,9 +135,39 @@ function saveGraphInNodeAndEdgesFormat(TARGET_DIR, graphNodesEdges) {
         }
 }
 
+function saveCyle(TARGET_DIR, cycle) {
+     const repoPath = path.join(newRepo, path.basename(TARGET_DIR))
+    const fullRepoPath = path.join(repoPath, 'cycle.json')
+
+    if (!fs.existsSync(repoPath)) {
+            fs.mkdir(repoPath, { recursive: true }, (err) => {
+                if (err) {
+                    console.error('Error creating directory:', err);
+                } else {
+                    console.log('Directory created successfully!');
+                }
+            });
+        } else {
+            console.log('Directory already exists. Using existing directory...');
+        }
+    if (fs.existsSync(fullRepoPath)) {
+            console.log('File already exists. Overwriting...');
+        } else {
+
+            fs.writeFile(fullRepoPath, JSON.stringify(cycle, null, 2), (err) => {
+                if (err) {
+                    console.error('Error writing file:', err);
+                } else {
+                    console.log('Results saved successfully!');
+                }
+            });
+        }
+}
+
 export { 
     saveGraph, 
     saveInsights, 
     saveReadableInsights,
-    saveGraphInNodeAndEdgesFormat
+    saveGraphInNodeAndEdgesFormat,
+    saveCyle
 }
