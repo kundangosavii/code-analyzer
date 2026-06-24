8fd560c9bf00fef68eb27dafdd64bf3bc106023b
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-24 22:51:45 +0530

    implemeted main LLM service

---
+++ src/api/services/llmService.js
@@ -0,0 +1,22 @@
+import { hfService } from './hfService.js';
+import { buildPrompt } from './promptBuilder.js';
+
+export async function llmService(analysisData) {
+    try {
+        const prompt = buildPrompt(analysisData)
+
+        const llmResponse = await hfService(prompt);
+
+        const result =
+            llmResponse?.[0]?.generated_text ||
+            llmResponse?.generated_text ||
+            "No response from model";
+
+        return result;
+
+    } catch (error) {
+        console.error("Error in llmService:", error);
+        throw error;
+    }
+
+}
