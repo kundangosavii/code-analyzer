fe214668ab86d7b30355ab8179b58a59f0c004fb
Kundan Gosavi <gosavikundan4@gmail.com>
2026-06-25 00:15:51 +0530

    Implemented dummy code of calling HF model using HF sdk

--- src/api/services/hfService.js
+++ src/api/services/hfService.js
@@ -1,29 +1,45 @@
-import axios from 'axios';
 
-const HF_API_URL = 'https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2';
+import { InferenceClient } from "@huggingface/inference";
+
+// const HF_TOKEN='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'
+
 
 export async function hfService(prompt) {
-    try{
-        const response = await axios.post(
-            HF_API_URL,
-            {
-                input: prompt,
-                parameters : {
-                    temperature: 0.7,
-                    max_new_tokens: 500,
-                }
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${process.env.HF_API_KEY}`,
-                    contentType: 'application/json',
-                },
-            }
-        )
+    try {
+        const client = new InferenceClient(process.env.HF_TOKEN);
 
-        return response.data;
+        const chatCompletion = await client.chatCompletion({
+            model: "deepseek-ai/DeepSeek-V4-Pro:novita",
+            messages: [
+                {
+                    role: "user",
+                    content: "What is the capital of France?",
+                },
+            ],
+        });
+
+        console.log(chatCompletion.choices[0].message);
+
+        // const response = await axios.post(
+        //     HF_API_URL,
+        //     {
+        //         input: prompt,
+        //         parameters : {
+        //             temperature: 0.7,
+        //             max_new_tokens: 500,
+        //         }
+        //     },
+        //     {
+        //         headers: {
+        //             Authorization: `Bearer ${process.env.HF_API_KEY}`,
+        //             contentType: 'application/json',
+        //         },
+        //     }
+        // )
+
+        // return response.data;
     }
-    catch(error){
+    catch (error) {
         console.error("Error in hfService:", error);
         throw error;
     }
